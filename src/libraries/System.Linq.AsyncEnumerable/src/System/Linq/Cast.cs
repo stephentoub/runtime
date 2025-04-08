51320a1befe3c1d7e677fff0a48172b17a0da41f
Stephen Toub <stoub@microsoft.com>
2025-04-07 21:13:37 -0400

    implement cast

--- src/libraries/System.Linq.AsyncEnumerable/src/System/Linq/Cast.cs
+++ src/libraries/System.Linq.AsyncEnumerable/src/System/Linq/Cast.cs
@@ -10,34 +10,33 @@ namespace System.Linq
 {
     public static partial class AsyncEnumerable
     {
-        // TODO https://github.com/dotnet/runtime/issues/111717:
-        // Consider before shipping .NET 10 whether this can instead use extension everything to support any IAsyncEnumerable<T> source.
-        // Right now it's limited because you can't cast an IAsyncEnumerable<TValueType> to IAsyncEnumerable<object>. But the method with this
-        // shape is necessary to support query comprehensions with explicit types, e.g. `from string s in asyncEnumerable`.
-
-        /// <summary>
-        /// Casts the elements of an <see cref="IAsyncEnumerable{Object}"/> to the specified type.
-        /// </summary>
-        /// <typeparam name="TResult">The type to cast the elements of source to.</typeparam>
+        /// <summary>Provides extensions for <see cref="IAsyncEnumerable{TSource}"/>.</summary>
         /// <param name="source">The <see cref="IAsyncEnumerable{Object}"/> that contains the elements to be cast to type <typeparamref name="TResult"/>.</param>
-        /// <returns>An <see cref="IAsyncEnumerable{TResult}"/> that contains each element of the source sequence cast to the <typeparamref name="TResult"/> type.</returns>
-        public static IAsyncEnumerable<TResult> Cast<TResult>( // satisfies the C# query-expression pattern
-            this IAsyncEnumerable<object?> source)
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        extension<TSource>(IAsyncEnumerable<TSource> source)
         {
-            ThrowHelper.ThrowIfNull(source);
-
-            return
-                source.IsKnownEmpty() ? Empty<TResult>() :
-                source as IAsyncEnumerable<TResult> ??
-                Impl(source, default);
-
-            static async IAsyncEnumerable<TResult> Impl(
-                IAsyncEnumerable<object?> source,
-                [EnumeratorCancellation] CancellationToken cancellationToken)
+            /// <summary>
+            /// Casts the elements of an <see cref="IAsyncEnumerable{Object}"/> to the specified type.
+            /// </summary>
+            /// <typeparam name="TResult">The type to cast the elements of source to.</typeparam>
+            /// <returns>An <see cref="IAsyncEnumerable{TResult}"/> that contains each element of the source sequence cast to the <typeparamref name="TResult"/> type.</returns>
+            public IAsyncEnumerable<TResult> Cast<TResult>() // satisfies the C# query-expression pattern
             {
-                await foreach (object? item in source.WithCancellation(cancellationToken))
+                ThrowHelper.ThrowIfNull(source);
+
+                return
+                    source.IsKnownEmpty() ? Empty<TResult>() :
+                    source as IAsyncEnumerable<TResult> ??
+                    Impl(source, default);
+
+                static async IAsyncEnumerable<TResult> Impl(
+                    IAsyncEnumerable<TSource?> source,
+                    [EnumeratorCancellation] CancellationToken cancellationToken)
                 {
-                    yield return (TResult)item!;
+                    await foreach (object? item in source.WithCancellation(cancellationToken))
+                    {
+                        yield return (TResult)item!;
+                    }
                 }
             }
         }
